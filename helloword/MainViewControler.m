56dd71174a33f92c4b866c05a3907a0bc16500a4
yanruifeng <yanruifeng@syriusrobotics.com>
2025-03-27 20:57:57 +0800

    feat: UILabel组件使用详解

--- helloword/MainViewControler.m
+++ helloword/MainViewControler.m
@@ -8,18 +8,30 @@
 #import "MainViewControler.h"
 
 @interface MainViewControler ()
-@property (weak, nonatomic) IBOutlet UITextField *num2;
-@property (weak, nonatomic) IBOutlet UILabel *result;
-@property (weak, nonatomic) IBOutlet UITextField *num1;
-@property (weak, nonatomic) IBOutlet UIButton *btnCalc;
+@property (weak, nonatomic) IBOutlet UILabel *lbUserName;
 @end
 
 @implementation MainViewControler
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.num1.keyboardType = UIKeyboardTypeNumberPad;
-    self.num2.keyboardType = UIKeyboardTypeNumberPad;
+
+    self.lbUserName.text=@"测试一行白鹭上青天，朝辞白帝彩云间";
+    self.lbUserName.lineBreakMode=NSLineBreakByTruncatingTail;
+    self.lbUserName.textColor=[UIColor redColor];
+    self.lbUserName.frame= CGRectMake(10, 70, 500, 100);
+    self.lbUserName.backgroundColor =[UIColor orangeColor];
+    self.lbUserName.shadowOffset=CGSizeMake(0, -5);
+    self.lbUserName.shadowColor = [UIColor whiteColor];
+    UIImageView *userAvatar = [[UIImageView alloc] init];
+    userAvatar.backgroundColor =[UIColor greenColor];
+    userAvatar.frame =CGRectMake(50, 50, 200, 100);
+    UIImage *image = [UIImage imageNamed:@"ic_live"];
+    userAvatar.image = image;
+    userAvatar.contentMode = UIViewContentModeScaleAspectFill;
+    userAvatar.clipsToBounds =YES;
+    [self.view addSubview:userAvatar];
+    
 }
 /*
 #pragma mark - Navigation
@@ -30,35 +42,5 @@
     // Pass the selected object to the new view controller.
 }
 */
-- (IBAction)calcResult:(UIButton *)sender {
-    NSString *num1 = self.num1.text;
-    NSString *num2 = self.num2.text;
-    if(num1.length==0){
-//        dispatch_async(dispatch_get_main_queue(), ^{
-            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"确定要退出嘛?" message:@"显示的信息" preferredStyle:UIAlertControllerStyleAlert];
-            UIAlertAction *action = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-                    NSLog(@"点击了取消");
-            }];
-            UIAlertAction *action1 = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
-                    NSLog(@"点击了确定");
-                    [self.navigationController popViewControllerAnimated:YES];
-            }];
-           
-            [alertController addAction:action];
-            [alertController addAction:action1];
-//        });
-        [self presentViewController:alertController animated:YES completion:nil];
-        NSLog(@"请输入第一个数字");
-        
-        return;
-    }
-    if(num2.length==0){
-        NSLog(@"请输入第二个数字");
-        return;
-    }
-    NSInteger result = [num1 integerValue] + [num2  integerValue];
-    self.result.text = [NSString stringWithFormat:@"%ld",result];
-    
-}
 
 @end
