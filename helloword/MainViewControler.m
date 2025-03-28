8dc53977bb4956f25c295f1e2ed47c137debd35a
yanruifeng <yanruifeng@syriusrobotics.com>
2025-03-28 23:03:31 +0800

    feat: 学习imageName 与imageWithContentsOfFile区别

--- helloword/MainViewControler.m
+++ helloword/MainViewControler.m
@@ -8,30 +8,47 @@
 #import "MainViewControler.h"
 
 @interface MainViewControler ()
-@property (weak, nonatomic) IBOutlet UILabel *lbUserName;
+@property(nonatomic,strong) UIButton *btnLoad;
+@property(nonatomic,strong) UIButton *btnDestroy;
+@property(nonatomic,strong) NSMutableArray<UIImage *> *images;
 @end
 
 @implementation MainViewControler
-
+- (NSMutableArray<UIImage *> *)images{
+    if(!_images){
+        _images =[NSMutableArray array];
+    }
+    return  _images;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    self.lbUserName.text=@"测试一行白鹭上青天，朝辞白帝彩云间";
-    self.lbUserName.lineBreakMode=NSLineBreakByTruncatingTail;
-    self.lbUserName.textColor=[UIColor redColor];
-    self.lbUserName.frame= CGRectMake(10, 70, 500, 100);
-    self.lbUserName.backgroundColor =[UIColor orangeColor];
-    self.lbUserName.shadowOffset=CGSizeMake(0, -5);
-    self.lbUserName.shadowColor = [UIColor whiteColor];
-    UIImageView *userAvatar = [[UIImageView alloc] init];
-    userAvatar.backgroundColor =[UIColor greenColor];
-    userAvatar.frame =CGRectMake(50, 50, 200, 100);
-    UIImage *image = [UIImage imageNamed:@"ic_live"];
-    userAvatar.image = image;
-    userAvatar.contentMode = UIViewContentModeScaleAspectFill;
-    userAvatar.clipsToBounds =YES;
-    [self.view addSubview:userAvatar];
+    self.btnLoad =[[UIButton alloc]init];
+       self.btnDestroy =[[UIButton alloc] initWithFrame:(CGRect){{100,300},{80,30}}];
+       [self.btnDestroy setTitle:@"销毁" forState:UIControlStateNormal];
+       [self.btnDestroy addTarget:self action:@selector(btnReleaseMemory) forControlEvents:UIControlEventTouchUpInside];
+    [self.btnDestroy setBackgroundColor:[UIColor redColor]];
+       self.btnLoad.frame= CGRectMake(20, 20, 100, 100);
+       self.btnLoad.backgroundColor=[UIColor greenColor];
+       [self.view addSubview:self.btnDestroy];
+       [self.btnLoad setTitle:@"点击我" forState:UIControlStateNormal];
+       [self.btnLoad addTarget:self
+                   action:@selector(loadImage)  // 关联方法
+        forControlEvents:UIControlEventTouchUpInside]; // 事件类型
+       [self.view addSubview:self.btnLoad];
+}
+- (void) loadImage{
+    for (int i =0; i<3; i++) {
+//        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%d",i+1]];
+        UIImage *image = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%d",i+1] ofType:@"png"]];
+        [self.images addObject:image];
+    }
+    NSLog(@"走了呀%zd",self.images.count);
     
+}
+- (void) btnReleaseMemory{
+    [self.images removeAllObjects];
+    NSLog(@"销毁%zd",self.images.count);
+    self.images = nil;
 }
 /*
 #pragma mark - Navigation
