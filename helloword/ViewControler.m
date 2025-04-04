08a54ae8c2e08ed2ea525d0865ea9e86584e16a2
yanruifeng <yanruifeng@syriusrobotics.com>
2025-04-04 19:38:28 +0800

    feat: 不能直接通过first object lastObject 获取scrollview子组件 除非禁用掉滚动条，如果多个元素也不行啊

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -14,16 +14,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIView *redView = [[UIView alloc] init];
-    [redView setBackgroundColor:[UIColor redColor]];
-    redView.frame =CGRectMake(0, 0, 50, 50);
-    [self.scrollView addSubview:redView];
-    self.scrollView.clipsToBounds = YES;
-    //contentSize 尺寸小于等于scrollview尺寸不能滚动
-    self.scrollView.contentSize =CGSizeMake(301, 201);
-//    self.scrollView.scrollEnabled =NO;
-#pragma scrollview以及内部所有的子控件都不能给用户交互 ，点击/拖拽所有操作都不能响应
-    self.scrollView.userInteractionEnabled=NO;
+//    UIActivityIndicatorView *activityIndicator =[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
+//    activityIndicator.center = CGPointMake(self.scrollView.bounds.size.width*.5, -40);
+//    [activityIndicator startAnimating];
+//    [self.scrollView addSubview:activityIndicator];
+    UIImageView *imageView =[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"jieping.jpg"]];
+    [self.scrollView addSubview:imageView];
+    self.scrollView.contentSize =imageView.bounds.size;
+//    self.scrollView.showsVerticalScrollIndicator = NO;
+//    self.scrollView.showsHorizontalScrollIndicator  = NO;
+
+#pragma  注意点: 千万不要通过索引取 subviews数组 访问ScrollView子控件
+//     UIView *view =[self.scrollView viewWithTag:1];
+//    UIImageView *imgView = (UIImageView *)view;
+//    if(imgView){
+//        [imgView removeFromSuperview ];
+//    }
+    
+    //禁止弹簧效果
+//    self.scrollView.bounces = NO;
+    //若不设置contentSize不能滚动，但是他总是有弹簧效果的用户希望下拉刷新，-----------------------------
+//    self.scrollView.alwaysBounceVertical =YES;
+//    self.scrollView.alwaysBounceHorizontal =YES;
 }
 
 @end
