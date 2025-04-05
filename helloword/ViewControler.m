803408741ce11844a2ab7f61f1a9605c34e585f9
yanruifeng <yanruifeng@syriusrobotics.com>
2025-04-05 22:05:23 +0800

    fix: 优化

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -11,8 +11,11 @@
  苹果为什么把代理设置成weak 防止出现循环引用，导致内存泄漏
  代理监听控件的某些行为 代理
  */
-@interface ViewController () <UITextFieldDelegate>
-@property (weak, nonatomic) IBOutlet UITextField *txtInput;
+@interface ViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property(weak,nonatomic) UIImageView *imageView;
 @property(nonatomic,strong) XMGCar *car;
 @end
 
@@ -23,54 +26,32 @@
     self.car =[[XMGCar alloc] init];
     //id类型 任何oc类型的对象 万物皆对象
     //通过代码创建的scrollView 一开始subviews这个数组为nill
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 80, 300, 200)];
-    scrollView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:scrollView];
+//    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 80, 300, 200)];
+    self.scrollView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.scrollView];
     UIImage *image = [UIImage imageNamed:@"jieping.jpg"];
     UIImageView *imageVew =[[UIImageView alloc] initWithImage:image];
-    [scrollView addSubview:imageVew];
-    scrollView.contentSize =image.size;
+    [self.scrollView addSubview:imageVew];
+    self.imageView = imageVew;
+    self.scrollView.contentSize =image.size;
+    self.scrollView.minimumZoomScale =0.5;
+    self.scrollView.maximumZoomScale = 2.0;
+//    self.scrollView.delegate = self;
+    //可以通过storyboard拖拽设置代理
+//    self.scrollView.delegate = self;
     //根据oc ARC法则 一旦对象创建出来那一刻若没有强指针引用它 就会被销毁掉
-    scrollView.delegate =self.car;
-    //TextField比较特殊 技可以通过addTarget 也可以通过代理
-    self.txtInput.delegate = self;
-//    [self.txtInput addTarget:self action:@selector(beginEditing) forControlEvents:UIControlEventEditingDidBegin];
-//    [self.txtInput addTarget:self action:@selector(endEditing) forControlEvents:(UIControlEvents)UIControlEventEditingDidEnd];
-//    [self.txtInput addTarget:self action:@selector(onChangeText:) forControlEvents:UIControlEventEditingChanged];
-    
+//    scrollView.delegate =self.car;
 }
-#pragma mark - 1.通过代理 代理方法都是控件名开头的
--(void) textFieldDidBeginEditing:(UITextField *)textField{
-    NSLog(@"开始编辑");
+/**
+ 返回需要缩放的子控件（scrollView的子控件）
+ */
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    NSLog(@"%@",self.imageView);
+    return self.imageView;
 }
--(void) textFieldDidEndEditing:(UITextField *)textField{
-    NSLog(@"结束编辑");
+- (IBAction)btnClick:(UIButton *)sender {
+    NSLog(@"%@",self.imageView);
+    self.imageView.frame =CGRectMake(0, 0, 20, 20);
 }
-//-(void) textFieldDidChangeSelection:(UITextField *)textField{
-//    NSLog(@"内容改变1-----%@",textField.text);
-//}
-- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
-    NSLog(@"内容改变0-----%@",textField.text);
-    if([string isEqualToString:@"1"]){
-        return NO;
-    }
-    return YES;
-}
--(void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    //退出键盘
-//    [self.txtInput endEditing:YES];
-    //不再成为第一响应者意味着退出键盘-----, 第一响应者可以调出键盘
-//    [self.txtInput resignFirstResponder];
-    [self.view endEditing:YES];  //这个常用
-}
-#pragma mark -2.通过addTarget
--(void)beginEditing{
-    NSLog(@"开始编辑----");
-}
--(void)endEditing{
-    NSLog(@"结束编辑----");
-}
--(void)onChangeText:(UITextField *) textFiled{
-    NSLog(@"内容改变------%@",textFiled.text);
-}
+
 @end
