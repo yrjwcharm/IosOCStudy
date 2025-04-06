68d6bb8857bc494c8847cebe80e0564b5494f3d4
yanruifeng <yanruifeng@syriusrobotics.com>
2025-04-06 19:47:39 +0800

    fix: 自定义控件

---
+++ helloword/XMGPageView.h
@@ -0,0 +1,16 @@
+//
+//  XMGPageView.h
+//  helloword
+//
+//  Created by 闫瑞锋 on 2025/4/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XMGPageView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ helloword/XMGPageView.m
@@ -0,0 +1,20 @@
+//
+//  XMGPageView.m
+//  helloword
+//
+//  Created by 闫瑞锋 on 2025/4/6.
+//
+
+#import "XMGPageView.h"
+
+@implementation XMGPageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
