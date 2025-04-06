95171e8c974252989f568daf700f838dd1695639
yanruifeng <yanruifeng@syriusrobotics.com>
2025-04-06 15:06:04 +0800

    fix: 实现ScrollView循环滚动效果

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -20,8 +20,12 @@
 @implementation ViewController
 - (NSArray *)array {
     if (!_array) {
-        // 在原始数组前后各加一个元素实现循环
-        _array = @[@"1.jpg", @"2.jpg", @"3.jpg", @"4.jpg", @"5.jpg"];
+        NSArray *originalArray = @[@"1.jpg", @"2.jpg", @"3.jpg", @"4.jpg", @"5.jpg"];
+        NSMutableArray *tempArray = [NSMutableArray array];
+        [tempArray addObject:originalArray.lastObject];
+        [tempArray addObjectsFromArray:originalArray];
+        [tempArray addObject:originalArray.firstObject];
+        _array = [tempArray copy];
     }
     return _array;
 }
@@ -29,46 +33,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.scrollView.delegate = self; // 确保设置代理
-    
+    self.scrollView.delegate = self;
     CGFloat scrollViewW = self.scrollView.frame.size.width;
     CGFloat scrollViewH = self.scrollView.frame.size.height;
     
-    // 添加所有图片视图
     for (int i = 0; i < self.array.count; i++) {
         UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:self.array[i]]];
         imageView.frame = CGRectMake(i * scrollViewW, 0, scrollViewW, scrollViewH);
         [self.scrollView addSubview:imageView];
     }
+    
     self.scrollView.contentSize = CGSizeMake(self.array.count * scrollViewW, 0);
     self.scrollView.pagingEnabled = YES;
-    self.pageControl.numberOfPages = self.array.count;
-    self.pageControl.enabled =NO;
-#pragma  mark - .m文件成员变量是私有的，你能直接给它赋值吗？  这里就要用到KVC强大之处  私有的公共的都可以赋值
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    [self.scrollView setContentOffset:CGPointMake(scrollViewW, 0)];
     
-//    self.pageControl.userInteractionEnabled = NO;
-    if(self.array.count==1){
-//        self.pageControl.hidden =YES;//UIView的方法 或者更改alpha值
-//        self.pageControl.alpha=0;
-        //单页的时候隐藏
-        self.pageControl.hidesForSinglePage =YES;
+    self.pageControl.numberOfPages = self.array.count - 2;
+    self.pageControl.hidesForSinglePage = YES;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat scrollViewW = scrollView.frame.size.width;
+    CGFloat offsetX = scrollView.contentOffset.x;
+    
+    if (offsetX <= 0) {
+        [scrollView setContentOffset:CGPointMake(scrollViewW * (self.array.count - 2), 0) animated:NO];
     }
-    // 设置内容尺寸为7页
+    
+    if (offsetX >= scrollViewW * (self.array.count - 1)) {
+        [scrollView setContentOffset:CGPointMake(scrollViewW, 0) animated:NO];
+    }
+    
+    int page = (int)(scrollView.contentOffset.x / scrollViewW + 0.5) - 1;
+    self.pageControl.currentPage = page;
 }
-//-(void) scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
-//    if(decelerate==NO){
-//    }
-//}
-//#pragma mark - 1、当scrollView减速完毕后，才变更指示器
-//- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-//    //计算页码 偏移量的x/scrollView自己的宽度
-//    int page = self.scrollView.contentOffset.x/scrollView.frame.size.width;
-//    self.pageControl.currentPage = page;
-//}
-//#pragma mark - 2、当scrollView页面超过一半时，就变更指示器
--(void) scrollViewDidScroll:(UIScrollView *)scrollView{
-    //四舍五入 int(小数+0.5)
-        int page = self.scrollView.contentOffset.x/scrollView.frame.size.width+.5;
-        self.pageControl.currentPage = page;
-}
+
 @end
