cd447b25d58eb73a9a4116ccf7c091af20aa98dc
yanruifeng <yanruifeng@syriusrobotics.com>
2025-04-06 13:40:38 +0800

    feat: 解决ScrollView循环滚动 分页 swiper轮播展示效果

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -12,46 +12,54 @@
  代理监听控件的某些行为 代理
  */
 @interface ViewController () <UIScrollViewDelegate>
-@property (weak, nonatomic) IBOutlet UIButton *button;
-
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property(weak,nonatomic) UIImageView *imageView;
-@property(nonatomic,strong) XMGCar *car;
+@property(strong,nonatomic) NSArray *array;
 @end
 
 @implementation ViewController
-//只是代表控制器View加载完毕
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.car =[[XMGCar alloc] init];
-    //id类型 任何oc类型的对象 万物皆对象
-    //通过代码创建的scrollView 一开始subviews这个数组为nill
-//    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 80, 300, 200)];
-    self.scrollView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:self.scrollView];
-    UIImage *image = [UIImage imageNamed:@"jieping.jpg"];
-    UIImageView *imageVew =[[UIImageView alloc] initWithImage:image];
-    [self.scrollView addSubview:imageVew];
-    self.imageView = imageVew;
-    self.scrollView.contentSize =image.size;
-    self.scrollView.minimumZoomScale =0.5;
-    self.scrollView.maximumZoomScale = 2.0;
-//    self.scrollView.delegate = self;
-    //可以通过storyboard拖拽设置代理
-//    self.scrollView.delegate = self;
-    //根据oc ARC法则 一旦对象创建出来那一刻若没有强指针引用它 就会被销毁掉
-//    scrollView.delegate =self.car;
-}
-/**
- 返回需要缩放的子控件（scrollView的子控件）
- */
--(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
-    NSLog(@"%@",self.imageView);
-    return self.imageView;
-}
-- (IBAction)btnClick:(UIButton *)sender {
-    NSLog(@"%@",self.imageView);
-    self.imageView.frame =CGRectMake(0, 0, 20, 20);
+- (NSArray *)array {
+    if (!_array) {
+        // 在原始数组前后各加一个元素实现循环
+        _array = @[@"5.jpg", @"1.jpg", @"2.jpg", @"3.jpg", @"4.jpg", @"5.jpg", @"1.jpg"];
+    }
+    return _array;
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.scrollView.delegate = self; // 确保设置代理
+    
+    CGFloat scrollViewW = self.scrollView.frame.size.width;
+    CGFloat scrollViewH = self.scrollView.frame.size.height;
+    
+    // 添加所有图片视图
+    for (int i = 0; i < self.array.count; i++) {
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:self.array[i]]];
+        imageView.frame = CGRectMake(i * scrollViewW, 0, scrollViewW, scrollViewH);
+        [self.scrollView addSubview:imageView];
+    }
+    
+    // 设置内容尺寸为7页
+    self.scrollView.contentSize = CGSizeMake(scrollViewW * 7, 0);
+    self.scrollView.pagingEnabled = YES;
+    
+    // 初始定位到第二页（索引1，显示原始第一张图）
+    [self.scrollView setContentOffset:CGPointMake(scrollViewW * 1, 0)];
+}
+
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    CGFloat pageWidth = scrollView.frame.size.width;
+    NSInteger currentPage = scrollView.contentOffset.x / pageWidth;
+    
+    // 当滚动到第一个克隆页时跳转到真实最后一个页
+    if (currentPage == 0) {
+        [scrollView setContentOffset:CGPointMake(pageWidth * 5, 0) animated:NO];
+    }
+    // 当滚动到最后一个克隆页时跳转到真实第一个页
+    else if (currentPage == self.array.count - 1) {
+        [scrollView setContentOffset:CGPointMake(pageWidth * 1, 0) animated:NO];
+    }
+}
 @end
