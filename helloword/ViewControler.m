ffcaf7a8186eb6ca5a889690d92b5b8fb73ff0b6
yanruifeng <yanruifeng@syriusrobotics.com>
2025-04-12 20:11:05 +0800

    feat: 使用VFL语言添加约束支持

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -8,16 +8,23 @@
 #import "ViewControler.h"
 
 @interface MainViewControler ()
-@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *redW;
 @end
 
 @implementation MainViewControler
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
 }
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    self.label.text =@"sss";
-    
+    self.redW.constant = 50;
+    [UIView animateWithDuration:2 animations:^{
+        [self.view layoutIfNeeded]; //强制刷新
+        //直接放置内部执行 不会有动画 因为他是NSLayoutConstraint约束对象 不是UIView
+//        self.redW.constant = 50;
+        //只有先更改约束 再调用 [self.view layoutIfNeeded];才可
+
+    }];
 }
 @end
