83ba86b6f0b927faf34c695ac518a733af48f96d
yanruifeng <yanruifeng@syriusrobotics.com>
2025-04-20 18:04:58 +0800

    fix: 自定义static cell管理也需要cell类

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -5,20 +5,17 @@
 //  Created by 闫瑞锋 on 2025/3/23.
 //
 //不需要遵守协议、设置数据源
-#import "ViewControler.h"@interface ViewControler ()
+#import "ViewControler.h"
+@interface ViewControler ()
 
 @end
 
 @implementation ViewControler
-NSString *ID =@"cycler";
+
 - (void)viewDidLoad{
     [super viewDidLoad];
 }
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 20;
-}
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:ID];
-    return cell;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"点击%ld",indexPath.row);
 }
 @end
