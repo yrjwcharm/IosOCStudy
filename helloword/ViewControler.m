1d220b8905f7f5f7c270c561d6181a9d8a3c01c9
yanruifeng <yanruifeng@syriusrobotics.com>
2025-05-01 21:17:17 +0800

    fix: 完善列表功能

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -6,13 +6,35 @@
 //
 
 #import "ViewControler.h"
-
-@interface MainViewControler ()
+#import "XMGWineCell.h"
+#import "MJExtension.h"
+#import "XMGWine.h"
+@interface ViewControler () <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) NSArray *wineArray;
 @end
 
-@implementation MainViewControler
+@implementation ViewControler
+
+- (NSArray *)wineArray{
+    if (_wineArray == nil) {
+        _wineArray =[XMGWine mj_objectArrayWithFilename:@"wine.plist"];
+    }
+    return _wineArray;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *ID = @"cell";
+    XMGWineCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[XMGWineCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+    }
+    cell.wine = self.wineArray[indexPath.row];
+    return cell;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.wineArray.count;
 }
 @end
---
+++ helloword/XMGWineCell.h
@@ -0,0 +1,16 @@
+//
+//  XMGWineCell.h
+//  helloword
+//
+//  Created by 闫瑞锋 on 2025/5/1.
+//
+
+#import <UIKit/UIKit.h>
+@class XMGWine;
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XMGWineCell : UITableViewCell
+@property (nonatomic, strong) XMGWine *wine;
+@end
+
+NS_ASSUME_NONNULL_END
