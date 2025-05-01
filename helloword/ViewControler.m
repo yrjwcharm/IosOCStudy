56dd50c7ea35fcabe7e1c63232557d7f43d0b2e8
yanruifeng <yanruifeng@syriusrobotics.com>
2025-05-01 23:06:19 +0800

    feat:实现左滑删除功能

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -70,7 +70,9 @@
     return self.wineArray.count;
 }
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
-    
+    [self.wineArray removeObjectAtIndex:indexPath.row];
+    [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+
 }
 - (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath{
     return @"删除";
