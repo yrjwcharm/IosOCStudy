475d011502e5038024a6d9ea84e0896a62c8d6a4
yanruifeng <yanruifeng@syriusrobotics.com>
2025-05-02 11:29:27 +0800

    feat: 完善批量删除功能

--- helloword/ViewControler.m
+++ helloword/ViewControler.m
@@ -25,6 +25,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //告诉tabview在编辑模式下可以进行多选操作
+    [self.tableView setAllowsMultipleSelectionDuringEditing:YES];
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *ID = @"cell";
@@ -35,18 +37,16 @@
     cell.wine = self.wineArray[indexPath.row];
     return cell;
 }
-#pragma mark -添加数据
-- (IBAction)add:(UIButton *)sender {
-    XMGWine *win =[[XMGWine alloc] init];
-    win.name =@"新品上市";
-    win.image=@"head_course_icon.png";
-    win.price = @30;
-    [self.wineArray insertObject:win atIndex:0];
-    //全局刷新
-//    [self.tableView reloadData];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    
+#pragma mark -删除数据
+- (IBAction)remove:(UIButton *)sender {
+    //千万不要一边遍历，一边删除，因为每删除一个元素，其他元素的索引可能会发生变化
+    NSMutableArray *array =[NSMutableArray array];
+    for (int i =0; i<self.tableView.indexPathsForSelectedRows.count; i++) {
+        [array addObject:self.wineArray[i]];
+    }
+    [self.wineArray removeObjectsInArray:array];
+    [self.tableView reloadData];
+
 }
 #pragma mark -更新数据
 - (IBAction)update:(UIButton *)sender {
@@ -58,12 +58,11 @@
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
     [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
 }
-#pragma mark -删除数据
+#pragma mark -批量删除数据--
 
 - (IBAction)delete:(UIButton *)sender {
-    [self.wineArray removeObjectAtIndex:0];
-    //全局刷新
-    [self.tableView reloadData];
+//    self.tableView.editing =YES;
+    [self.tableView setEditing:!self.tableView.editing animated:YES];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -71,6 +70,8 @@
 }
 #pragma mark - UITableViewDelegate (滑动操作)
 - (UISwipeActionsConfiguration *)tableView:(UITableView *)tableView trailingSwipeActionsConfigurationForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //滑动菜单出现按钮操作时，table默认的编辑模式NO改成YES
+//        self.tableView.editing =NO;
     // 1. 删除操作
     UIContextualAction *deleteAction = [UIContextualAction contextualActionWithStyle:UIContextualActionStyleDestructive title:@"删除" handler:^(UIContextualAction * _Nonnull action, __kindof UIView * _Nonnull sourceView, void (^ _Nonnull completionHandler)(BOOL)) {
         // 从数据源中删除
@@ -90,7 +91,6 @@
         NSIndexPath *newIndexPath = [NSIndexPath indexPathForRow:0 inSection:indexPath.section];
         [tableView moveRowAtIndexPath:indexPath toIndexPath:newIndexPath];//底层逻辑已经包含了局部刷新功能
         completionHandler(YES);
-//        self.tableView.editing =NO;
     }];
     // 3. 返回配置
     UISwipeActionsConfiguration *config = [UISwipeActionsConfiguration configurationWithActions:@[deleteAction, pinAction]];
