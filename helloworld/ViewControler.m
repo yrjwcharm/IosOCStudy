f59b1d1599d72f4b88576dcffe906fb5f53c5d96
yanruifeng <yanruifeng@syriusrobotics.com>
2025-05-03 13:45:17 +0800

    feat: 重命名

---
+++ helloworld/ViewControler.m
@@ -0,0 +1,19 @@
+//
+//  MainViewControler.m
+//  helloword
+//
+//  Created by 闫瑞锋 on 2025/3/23.
+//
+
+#import "ViewControler.h"
+#import "MJExtension.h"
+@interface ViewControler ()
+@end
+
+@implementation ViewControler
+
+- (void) viewDidLoad{
+    [super viewDidLoad];
+}
+
+@end
